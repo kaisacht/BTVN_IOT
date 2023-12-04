32afa974a4490a05dfd3de20f1dddf2cfb38fb95
kaisacht <leanhvut1k27cht@gmail.com>
2023-12-04 08:34:33 +0700

    btt7

---
+++ BTT7/WA/Controllers/SensorDatasController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WA.Models;
+
+namespace WA.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SensorDatasController : ControllerBase
+    {
+        private readonly WebAPIDemoContext _context;
+
+        public SensorDatasController(WebAPIDemoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/SensorDatas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<SensorData>>> GetSensorDatas()
+        {
+            return await _context.SensorDatas.ToListAsync();
+        }
+
+        // GET: api/SensorDatas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SensorData>> GetSensorData(int id)
+        {
+            var sensorData = await _context.SensorDatas.FindAsync(id);
+
+            if (sensorData == null)
+            {
+                return NotFound();
+            }
+
+            return sensorData;
+        }
+
+        // PUT: api/SensorDatas/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSensorData(int id, SensorData sensorData)
+        {
+            if (id != sensorData.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(sensorData).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SensorDataExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/SensorDatas
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<SensorData>> PostSensorData(SensorData sensorData)
+        {
+            _context.SensorDatas.Add(sensorData);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSensorData", new { id = sensorData.Id }, sensorData);
+        }
+
+        // DELETE: api/SensorDatas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSensorData(int id)
+        {
+            var sensorData = await _context.SensorDatas.FindAsync(id);
+            if (sensorData == null)
+            {
+                return NotFound();
+            }
+
+            _context.SensorDatas.Remove(sensorData);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SensorDataExists(int id)
+        {
+            return _context.SensorDatas.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ BTT7/WA/Models/SensorData.cs
@@ -0,0 +1,13 @@
+﻿namespace WA.Models
+{
+    public class SensorData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Value { get; set; }
+
+        public DateTime ReceiveTime { get; set; }
+
+
+    }
+}
