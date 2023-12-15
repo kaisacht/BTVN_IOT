73d33c26b708e517b6b90e6016335cc3791d8900
kaisacht <leanhvut1k27cht@gmail.com>
2023-12-15 10:48:24 +0700

    add bao cao tong hop tuan 7

--- BTT7/WA/Models/SensorData.cs
+++ BTT7/WA/Models/SensorData.cs
@@ -5,9 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public float Value { get; set; }
-
         public DateTime ReceiveTime { get; set; }
-
-
     }
 }
--- BTT7/WA/Models/WebAPIDemoContext.cs
+++ BTT7/WA/Models/WebAPIDemoContext.cs
@@ -1,18 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-
 namespace WA.Models
 {
     public class WebAPIDemoContext : DbContext
     {
         public WebAPIDemoContext(DbContextOptions<WebAPIDemoContext> options) : base(options) { }
         public DbSet<WA.Models.SensorData> SensorDatas { get; set; }
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=Application.db;Cache=Shared");
             base.OnConfiguring(optionsBuilder);
-
         }
     }
 }
